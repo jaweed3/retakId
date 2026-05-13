8bc4e275329ea682c3ddeebbdfcf621d07349eba
FarrelGhozy <farrelafif05@gmail.com>
2026-05-13 16:53:42 +0700

    perbaiakn scorol kenapa retakan

--- web-app/src/components/Layout.tsx
+++ web-app/src/components/Layout.tsx
@@ -90,7 +90,7 @@ export function Layout() {
         </header>
 
         {/* Page content */}
-        <div className="flex-1 overflow-auto">
+        <div className="flex-1 overflow-auto scroll-smooth">
           <Outlet />
           <Footer />
         </div>
